20bfac664b5a61158284689955d3ddbaa4e5dda9
Donghyeon Kim <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-03-20 23:39:44 +0900

    Add files via upload

---
+++ example_source/CookieIsDelicious.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+chrome_driver_path = r"C:\Users\user\Documents\Python\chromedriver.exe"
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+driver.get('https://nozomi.la/post/25152416.html#sinensian')
+driver.implicitly_wait(5)
+
+savedCookies = driver.get_cookies()
+print('첫번째 쿠키 : ')
+print(savedCookies)
+print('\n\n')
+
+driver2 = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+driver2.get('https://nozomi.la/post/25152416.html#sinensian')
+driver2.delete_all_cookies()
+for cookie in savedCookies:
+    driver2.add_cookie(cookie)
+
+driver2.get('https://nozomi.la/post/25152416.html#sinensian')
+driver.implicitly_wait(3)
+print('두번째 쿠키 : ')
+print(driver2.get_cookies())
