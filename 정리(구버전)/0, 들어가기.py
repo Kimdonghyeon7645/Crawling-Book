e8210dbc06485eee0e9eb55475a9c7cf1eaaf6d5
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-12 19:17:36 +0900

    docs : move to folder

---
+++ 정리(구버전)/0, 들어가기.py
@@ -0,0 +1,9 @@
+"""
+ 우선 import할 패키지를 설치해 주도록 합시다.
+ C:\Users\user\AppData\Local\Programs\Python\Python37-32\Scripts
+ cmd로 위 링크를 들어가서 (cd ~)
+ pip install BeautifulSoup4    # BeautifulSoup 다운
+ pip install requests    # requests 다운
+ pip install selenium    # selenium 다운
+ 해줍니다!
+"""
