38756a95bc52e7c6c19ce94009131410475713a9
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-15 21:58:55 +0900

    code : finish update [대작] 프로그래머스 알고리즘 문제 크롤링.py

--- _Script/[대작] 프로그래머스 알고리즘 문제 크롤링.py
+++ _Script/[대작] 프로그래머스 알고리즘 문제 크롤링.py
@@ -49,9 +49,11 @@ def get_all_questions():
 
 def get_question_content(q_url):
     html = BeautifulSoup(requests.get(q_url).text, 'html.parser')
-    title = html.select_one("#tab > li").text
-    with open(PATH + title + ".md", "wb") as f:
-        f.write(html.select_one("#tour2").text)
+    title = html.select_one("#tab > li").text.split('\n')[1].strip().replace('?', '')
+    with open(PATH + title + ".md", "wt", encoding="utf-8") as f:
+        f.write("바로가기 : " + q_url + "\n")
+        f.write(str(html.select_one("#tour2")))
+    print(f"문제 '{title}' 다운로드 완료")
 
 
 if __name__ == '__main__':
@@ -66,6 +68,10 @@ if __name__ == '__main__':
     driver.get(url)
     filter_tag()
     get_all_questions()
+
+    count = 0
     for q in question_list:
         get_question_content(header_url + q)
+        count += 1
+    print(f"총 {count}건의 문제가 파일로 저장되었습니다. 프로그램을 정상 종료합니다...")
     driver.close()
