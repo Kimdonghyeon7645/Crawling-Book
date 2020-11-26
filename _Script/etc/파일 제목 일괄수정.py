46c8d13814ec28e7f3fe460e953a8cf2b6fbbb0b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-26 15:55:29 +0900

    code : add 파일 제목 일괄수정.py

---
+++ _Script/etc/파일 제목 일괄수정.py
@@ -0,0 +1,8 @@
+import os
+
+path = r"C:\Users\user\Downloads\OST_문명6"
+for old_name in os.listdir(path):
+    new_name = old_name.replace("y2mate.com - ", "")
+    os.rename(os.path.join(path, old_name), os.path.join(path, new_name))
+    if old_name != new_name:
+        print(f"{old_name:<100} -> {new_name:<100}으로 이름 변경")
