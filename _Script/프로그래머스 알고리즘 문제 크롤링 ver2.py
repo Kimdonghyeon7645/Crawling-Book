e4ded62c1eae54cb6a21a2505c4379a3ffbdbb1f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-29 23:30:48 +0900

    code : update 프로그래머스 알고리즘 문제 크롤링 ver2.py

--- _Script/프로그래머스 알고리즘 문제 크롤링 ver2.py
+++ _Script/프로그래머스 알고리즘 문제 크롤링 ver2.py
@@ -48,23 +48,23 @@ def get_all_questions_url():
         print("완료")
 
 
-def get_question_content(q_url: str):
+def get_question_content(q_url: str) -> int:
     html = BeautifulSoup(requests.get(q_url).text, 'html.parser')
     title = html.select_one("#tab > li").text.split('\n')[1].strip().replace('?', '')
-    if check_solved:
-
-        return
+    if check_solved and title in check_li:
+        print(f"{title} 문제는 이미 푼 문제입니다!")
+        return 0
     with open(PATH + title + ".md", "wt", encoding="utf-8") as f:
         f.write("프로그래머스 문제 바로가기 : " + q_url + "\n")
         f.write(str(html.select_one("#tour2")))
     print(f"문제 '{title}' 다운로드 완료")
+    return 1
 
 
 def get_all_questions_content():
     count = 0
     for q in question_list:
-        get_question_content(header_url + q)
-        count += 1
+        count += get_question_content(header_url + q)
     print(f"총 {count}건의 문제가 파일로 저장되었습니다. 프로그램을 정상 종료합니다...")
 
 
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     # 함수 순서대로 실행
     filter_tag(level=level)
     get_all_questions_url()
-    get_all_questions_content(True)
+    get_all_questions_content()
 
     # 셀레니움 종료
     driver.close()
